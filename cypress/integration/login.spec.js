503604d549ee3d45dbe69d8a98156f0accda73ee
eduardofroes95 <eduardofroes95@gmail.com>
2022-04-25 19:37:32 -0300

    Adicionando o arquivo de Login

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+context('Funcionalidade Login',() =>{
+
+    it('Deve fazer login' , () => {
+
+
+    })
+
+    it('Não deve fazer login' , () => {
+
+        
+
+    })
+})
