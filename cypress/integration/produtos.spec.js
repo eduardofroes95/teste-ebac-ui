af6059380b8f2f8535c8ff4e91577f98abb3a2d1
eduardofroes95 <eduardofroes95@gmail.com>
2022-04-27 20:13:20 -0300

    updates

--- cypress/integration/produtos.spec.js
+++ cypress/integration/produtos.spec.js
@@ -3,17 +3,25 @@ const faker = require('faker');
 
 describe('Produtos', () => {
 
-    beforeEach(() => {
+    before(() => {
         cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
     });
 
     it('Deve selecionar um produto da lista', () => {
         cy.get('[class="product-block grid"]').eq(3).click()
+        //last()
+        //first()
+        //contains('')
     });
-
+    
     it('Adicionar produto ao carrinho', () => {
-        
-
+        let quantidade = 12;
+        cy.get('.button-variable-item-S').click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.button-variable-item-Blue').click()
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.dropdown-toggle > .mini-cart-items').should('contain', quantidade)
+        cy.get('.woocommerce-message').should('contain', quantidade + ' × “Ajax Full-Zip Sweatshirt” foram adicionados no seu carrinho.')
     });
 
 
