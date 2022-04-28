ebf31f3d0274af6b44986a8dca5734f12eb3f931
eduardofroes95 <eduardofroes95@gmail.com>
2022-04-28 19:33:39 -0300

    updates

---
+++ cypress/integration/endereco.spec.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+const perfil = require('../fixtures/perfil.json')
+const faker = require('faker');
+
+context('Criar endereço', () => {
+
+
+it('Deve criar endereço', () => {
+    
+});
+
+
+
+
+    
+});
+
