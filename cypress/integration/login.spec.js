5c9c0068ef1f7040731002ada54cf9b9fb3ddd16
eduardofroes95 <eduardofroes95@gmail.com>
2022-04-28 18:32:41 -0300

    updates

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,18 +1,36 @@
 /// <reference types="cypress"/>
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade Login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-        
+        cy.visit('minha-conta/')
+
     });
-    
+
     it('Deve fazer login', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('contain', 'aluno_ebac')
     })
+
+    it('Deve fazer login utilizando dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+    });
+
+    it('Deve fazer login utilizando fixture', () => {
+
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+            cy.get('#password').type(dados.senha, {log: false})
+            cy.get('.woocommerce-form > .button').click()
+
+        })
+        
+    });
 
     it('Deve aparecer uma mensagem de senha incorreta', () => {
         cy.get('#username').type('aluno_ebac@teste.com')
@@ -25,6 +43,6 @@ context('Funcionalidade Login', () => {
         cy.get('#username').type('aluno_ebac@teste.comm')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.woocommerce-error > li').should('contain' , 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.' )
+        cy.get('.woocommerce-error > li').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
     })
 })
--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -6,7 +6,7 @@ const faker = require('faker');
 describe('Funcionalidade Pré-Cadastro', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
 
     });
 
@@ -16,7 +16,7 @@ describe('Funcionalidade Pré-Cadastro', () => {
         cy.get(':nth-child(4) > .button').click ()
         cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
         cy.get('#account_first_name').type(faker.name.firstName ())
-        cy.get('#account_last_name').type(faker.name.lastNgame ())
+        cy.get('#account_last_name').type(faker.name.lastName ())
         cy.get('.woocommerce-Button').click()
         cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso')
     });
